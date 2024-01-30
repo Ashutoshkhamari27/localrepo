071a69883ebbd11c51838cdc816986ed6c711076
Ashutosh Khamari <khamariashutosh27@gmail.com>
2024-01-30 09:09:36 +0530

    Added 2 more jave files

---
+++ Testing1.java
@@ -0,0 +1,8 @@
+package localRepo;
+
+public class Testing1 {
+    public static void main(String[] args) {
+        System.out.println("Hellooo");
+    }
+    
+}
---
+++ Testing2.java
@@ -0,0 +1,8 @@
+package localRepo;
+
+public class Testing2 {
+    public static void main(String[] args) {
+        System.out.println("Thekkoo testing 222");
+    }
+    
+}
